559a407dd628ce948efcb1cfd2832fccd5ef3ab4
Griff <Griff@griffins-air.devices.brown.edu>
2018-09-12 16:53:20 -0400

    griffin commit

---
+++ Griffin/shadoof.js
@@ -0,0 +1,16 @@
+
+// ——— P5 functions ———
+function setup() {
+  createCanvas(1024, 576);
+  background(0);
+  // noCursor();
+  // initialize(); // a js-function
+}
+    
+function draw() {
+  background(0);
+  fill(255);
+  text("The Text",100,100);
+}
+
+
