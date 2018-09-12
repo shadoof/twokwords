b264d6794fbf87fd60e35d51e1e3817d6a6fe125
ABuccellato <qwertybagel@gmail.com>
2018-09-12 16:51:08 -0400

    abuccell

---
+++ abuccell/shadoof.js
@@ -0,0 +1,16 @@
+
+// ——— P5 functions ———
+function setup() {
+  //size(1024, 576);
+  createCanvas(500,500);
+  background(0);
+  //noCursor();
+  //initialize(); // a js-function
+}
+
+function draw() {
+  background(0);
+  fill(100);
+  text("test",100,100);
+  //RiText.drawAll();
+}
