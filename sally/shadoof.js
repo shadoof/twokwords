adf0819f9546caf74f9a144b1168ebe0e774f6a7
cqx931 <cqx931@gmail.com>
2018-09-12 16:55:06 -0400

    sally's commit

---
+++ sally/shadoof.js
@@ -0,0 +1,12 @@
+// ——— P5 functions ———
+function setup() {
+  createCanvas(1024, 576);
+  background(0);
+  noCursor();
+  color(255)
+}
+
+function draw() {
+  background(0);
+  text("AM I HERE I AM");
+}
