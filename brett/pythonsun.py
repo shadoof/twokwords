1629b1fca4eb3e80ca38a9a6d438c18ab4f9c737
brettmzehner <brett_zehner@brown.edu>
2018-11-07 14:17:30 -0500

    anything

--- brett/pythonsun.py
+++ brett/pythonsun.py
@@ -1,45 +1,57 @@
 import re
 
-words = open("SUN.txt").read()
-words = re.compile('[\W]').sub('',words)
-
-words = re.split(r'(score+|word+)', words)
-
-wordScoreList = [] #WHERE EVERYTHING IS STORED
-
-flag = 0
-wordTemp = ""
+values_a = open("reflect.rtf").read() #VALUES TO BE LOOKED IT
+values_b = open("radiation.rtf").read()
+values_c = open("emission.rtf").read()
 
 class wordScore:
     def __init__(self,word,score):
         self.word = word
         self.score = score
 
-for word in words:
+def makeList(words):
+    words = re.compile('[\W]').sub('',words)
+    words = re.split(r'(score+|word+)', words)
+    wordScoreList = [] #
+    flag = 0
+    wordTemp = ""
 
+    for word in words:
+        if flag == 2:
+            wordTemp = word
+        elif flag ==1:
+            wordScoreList.append(wordScore(wordTemp,word))
 
-    if flag ==2:
-        wordTemp = word
-    elif flag ==1:
-        wordScoreList.append(wordScore(wordTemp,word))
+        if word == "word":
+            flag = 2
+        elif word == "score":
+            flag = 1
+        else:
+            flag = 0
 
-    if word == "word":
-        flag = 2
-    elif word == "score":
-        flag = 1
-    else:
-        flag = 0
+    return wordScoreList
 
-
-
-mainText = open("POEMS FOR THE ANTHROPOCENE.txt").read()
+categories = [makeList(values_a),makeList(values_b),makeList(values_c)]
+mainText = open("climatedecision.rtf").read() #MAIN TEXT TO BE READ THROUGH
+mainText = re.compile(r'[^a-zA-Z ]+').sub('',mainText)
 mainText = mainText.split(" ")
 
-totalScore = 0
 
-for word in mainText:
-    for match in wordScoreList:
-        if word == match.word:
-            totalScore += int(match.score)
+scores = [0,0,0]
+triplets = [[],[],[]]
 
-print(totalScore)
+for index,word in enumerate(mainText):
+    for cat,wordScoreList in enumerate(categories):
+        for match in wordScoreList:
+            if word == match.word:
+                scores[cat] += int(match.score)
+                triplets[cat].append(mainText[index-1]+" "+mainText[index]+" "+mainText[index+1]+'\n')
+
+files = [open("resultsA.txt","w+"),open("resultsB.txt","w+"),open("resultsC.txt","w+")]
+
+for index,triplet in enumerate(triplets):
+    for line in triplet:
+        files[index].write(line)
+    files[index].write("score:"+str(scores[index]))
+
+print("done")
